0121ad224733e6cd2ce45d722005b53017360777
Adria <avillacrosa@gmail.com>
2024-08-16 08:49:34 +0200

    Save what?

--- main.jl
+++ main.jl
@@ -1,8 +1,10 @@
 include("VTFM/src/VTFM.jl")
+
+
 using .VTFM
 println("Running!")
 @time begin
-n = 5;
+n = 40;
 ns = [n, n, n]
 ds = [1/(n-1), 1/(n-1), 1/(n-1)]
 uBC = [3 0 3 0; 3 0 1 0; 3 0 2 0; 3 1 3 0.1]
